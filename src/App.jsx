de64bd8c3a5b1d895bacc32ad59c92fbe6c1912a
Ayush-codes045 <bittukumar8083a@gmail.com>
2025-07-29 22:29:05 +0530

    kallu

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React+kallu is op</h1>
+      <h1>Vite + React+kallu is very op</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
